89ce93831d7786ef120db8fb6753538dc4590ea4
Oleg Ershov <oecorango@gmail.com>
2023-08-10 03:29:19 +0400

    feat: add validation form from create-page

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,40 +1,93 @@
 import { FieldValues, useForm } from 'react-hook-form';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { Message } from 'primereact/message';
 
 export const SignInPage = (): JSX.Element => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data: FieldValues): void =>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm({
+    mode: 'onBlur',
+  });
+  const onSubmit = (data: FieldValues): void => {
     console.log(JSON.stringify(data));
+    reset();
+  };
 
   return (
     <div className="registration__page content">
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-column">
         <InputText
-          className="mb-5"
-          {...register('email')}
-          type="email"
+          className="mb-1"
+          {...register('email', {
+            required: 'Required to fill',
+            minLength: {
+              value: 5,
+              message: 'Minimum 5 characters',
+            },
+            pattern: {
+              value:
+                // eslint-disable-next-line no-useless-escape
+                /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: 'Incorrect email',
+            },
+          })}
+          type="text"
           placeholder="Enter your email"
         />
+        <Message
+          className={
+            ((errors?.email?.message as string) && 'h-1rem') || 'hidden'
+          }
+          severity={'error'}
+          text={errors?.email?.message as string}
+        />
+
         <InputText
-          className="mb-5"
-          {...register('password')}
+          className="mt-5 mb-1"
+          {...register('password', {
+            required: 'Required to fill',
+            minLength: {
+              value: 8,
+              message: 'Minimum 8 characters',
+            },
+            maxLength: {
+              value: 20,
+              message: 'Maximum 20 characters',
+            },
+          })}
           type="password"
           placeholder="Enter your password"
           autoComplete="on"
         />
-        <Button className="mb-7" label="Sign In" type="submit" disabled />
+        <Message
+          className={
+            ((errors?.password?.message as string) && 'h-1rem') || 'hidden'
+          }
+          severity={'error'}
+          text={errors?.password?.message as string}
+        />
+
+        <Button
+          className="mt-6 mb-5"
+          label="Sign In"
+          type="submit"
+          disabled={!isValid}
+        />
       </form>
+
       <h4 className="center mb-2 pl-2 pr-2 text-center">
         If you are not registered, please register in our store.
       </h4>
+
       <Button
         className="mt-3 mb-8"
         label="Registration"
         type="button"
-        onClick={(): void => {
-          console.log('click');
-        }}
+        onClick={(e): void => console.log(e.nativeEvent)}
       />
     </div>
   );

f7fd91f53c73db9e54205010b4f47762bfca05db
volsvik <rudyard47@gmail.com>
2023-08-16 23:46:42 +0300

    feat: add billing and shipping addresses in RegistrationForm

--- src/components/Forms/RegistrationForm.tsx
+++ src/components/Forms/RegistrationForm.tsx
@@ -3,6 +3,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { ToggleButton, ToggleButtonChangeEvent } from 'primereact/togglebutton';
 import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
 import { InputMask, InputMaskChangeEvent } from 'primereact/inputmask';
 import { ICountriesData, IRegistrationForm } from '../../interface/interface';
@@ -12,7 +13,8 @@ import { validRegisterData } from '../../utils/validRegisterData';
 import { ErrorMessage } from '../ErrorMessage';
 
 let postCod: string = '_____';
-
+let ship: boolean = false,
+  bill: boolean = false;
 export const RegistrationForm = (props: {
   create: () => void;
 }): JSX.Element => {
@@ -29,12 +31,13 @@ export const RegistrationForm = (props: {
     null,
   );
   const countries: ICountriesData[] = countriesData;
-
+  const [checkedShip, setcheCkedShip] = useState<boolean>(false);
+  const [checkedBill, setCheckedBill] = useState<boolean>(false);
   const onSubmit: SubmitHandler<IRegistrationForm> = (
     data: IRegistrationForm,
   ): void => {
     data.postalCode = value;
-    takeDataForm(data);
+    takeDataForm(data, ship, bill);
     props.create();
   };
 
@@ -82,51 +85,75 @@ export const RegistrationForm = (props: {
         />
         <ErrorMessage err={errors} name={'dateOfBirth'} />
 
-        <InputText
-          className="mb-1"
-          {...register('streetName')}
-          placeholder="Enter your street"
-        />
-        <ErrorMessage err={errors} name={'streetName'} />
-
-        <InputText
-          className="mb-1"
-          {...register('city')}
-          placeholder="Enter your city"
-        />
-        <ErrorMessage err={errors} name={'city'} />
-
-        <div className="w-full mb-1">
-          <Dropdown
-            className="w-full"
-            {...register('country')}
-            value={selectedCountry}
-            onChange={(e: DropdownChangeEvent): void => {
-              setSelectedCountry(e.value);
-              postCod = e.value.postalCode;
-            }}
-            options={countries}
-            optionLabel="name"
-            placeholder="Select your Country"
+        <div className="registration_adress">
+          <InputText
+            className="mb-1"
+            {...register('streetName')}
+            placeholder="Enter your street"
           />
-          <ErrorMessage err={errors} name={'country'} />
-        </div>
+          <ErrorMessage err={errors} name={'streetName'} />
 
-        <label htmlFor="serial" className="registration_span">
-          Postcode (example = {postCod})
-        </label>
-        <div className="w-full mb-1">
-          <InputMask
-            className="w-full"
-            value={value}
-            onChange={(e: InputMaskChangeEvent): void => {
-              if (e.target.value) {
-                setValue(e.target.value);
-              }
-            }}
-            mask={postCod}
-            placeholder={postCod}
+          <InputText
+            className="mb-1"
+            {...register('city')}
+            placeholder="Enter your city"
           />
+          <ErrorMessage err={errors} name={'city'} />
+
+          <div className="w-full mb-1">
+            <Dropdown
+              className="w-full"
+              {...register('country')}
+              value={selectedCountry}
+              onChange={(e: DropdownChangeEvent): void => {
+                setSelectedCountry(e.value);
+                postCod = e.value.postalCode;
+              }}
+              options={countries}
+              optionLabel="name"
+              placeholder="Select your Country"
+            />
+            <ErrorMessage err={errors} name={'country'} />
+          </div>
+
+          <label htmlFor="serial" className="registration_span">
+            Postcode (example = {postCod})
+          </label>
+          <div className="w-full mb-1">
+            <InputMask
+              className="w-full"
+              value={value}
+              onChange={(e: InputMaskChangeEvent): void => {
+                if (e.target.value) {
+                  setValue(e.target.value);
+                }
+              }}
+              mask={postCod}
+              placeholder={postCod}
+            />
+          </div>
+          <label className="registration_span">Переключатель</label>
+          <label className="registration_span">
+            defaultShip&nbsp;&nbsp;&nbsp;defaultBill
+          </label>
+          <div className="registration_shipBill">
+            <ToggleButton
+              checked={checkedShip}
+              onChange={(e: ToggleButtonChangeEvent): void => {
+                setcheCkedShip(e.value);
+                ship = e.value;
+              }}
+              className="w-8rem"
+            />
+            <ToggleButton
+              checked={checkedBill}
+              onChange={(e: ToggleButtonChangeEvent): void => {
+                setCheckedBill(e.value);
+                bill = e.value;
+              }}
+              className="w-8rem"
+            />
+          </div>
         </div>
 
         <Button className="mt-3 mb-1" label="Registration" type="submit" />

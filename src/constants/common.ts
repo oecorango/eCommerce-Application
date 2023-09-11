838eb4af6d9bb2d6f00af0fc236251bb3bca3df2
Yahor Tsytsarkin <egormail88@gmail.com>
2023-09-11 15:14:05 +0300

    feat: filled out the about page

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,4 +1,32 @@
+import egor from '../assets/photo/egor.jpg';
 export const CENTS_PER_EURO = 100;
 export const FIRST_INDEX = 0;
 export const PRODUCTS_IN_PAGE = 9;
 export const POPULAR_PRODUCTS_IN_PAGE = 4;
+
+export const teamMembers = [
+  {
+    id: 0,
+    name: 'Егор',
+    role: 'Тим Лид / Full-stack Developer',
+    bio: 'Егор - тим лид нашей команды разработчиков. Он обладает богатым опытом в разработке и координации проектов. Егор активно участвовал в создании успешного продукта и руководил всей командой.',
+    githubProfile: 'https://github.com/egorca6',
+    photo: egor,
+  },
+  {
+    id: 1,
+    name: 'Олег',
+    role: 'Full-stack Developer',
+    bio: 'Олег - опытный Senior Developer в нашей команде. Он сделал значительный вклад в проект, ответственный за множество ключевых задач, включая настройку окружения разработки, основную страницу и маршрутизацию, а также валидацию ввода на странице входа и многое другое.',
+    githubProfile: '../../assets/404.png',
+    photo: egor,
+  },
+  {
+    id: 2,
+    name: 'Виктор',
+    role: 'Full-stack Developer',
+    bio: 'Виктор - опытный Full-stack Developer, который внес существенный вклад в наш проект. Он отвечал за настройку проекта и API с использованием CommerceTools, реализацию страницы корзины и детальной страницы продукта, а также интеграцию с данными пользовательских профилей.',
+    githubProfile: 'ссылка_на_GitHub_Виктора',
+    photo: egor,
+  },
+];
